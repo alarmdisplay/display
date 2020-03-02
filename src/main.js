2704daf456f06259c24bba8d73c8220b6b03523f
Andreas Brain <dev@abrain.de>
2020-03-02 18:04:36 +0100

    Set up a socket connection to the backend service

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
+import io from 'socket.io-client'
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/index.css';
 
-Vue.config.productionTip = false
+Vue.use(VueToast);
+
+Vue.config.productionTip = false;
+
+let socket;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  mounted: function () {
+    setupSocket();
+  }
+}).$mount('#app');
+
+function setupSocket() {
+  socket = io();
+
+  socket.on('connect_error', function() {
+    Vue.$toast.error('Could not connect to the server');
+  });
+
+  socket.on('reconnect_error', function() {
+    Vue.$toast.error('Could not reconnect to the server');
+  });
+
+  socket.on('reconnecting', function() {
+    Vue.$toast.success('Reconnected to server');
+  });
+
+  socket.on('test message', function(data) {
+    console.log('Received data', data);
+  });
+}
