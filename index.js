07659a590d11504587e3e85c7e5c95eefe757b24
Andreas Brain <dev@abrain.de>
2020-03-05 15:04:54 +0100

    Use log4js for logging

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const log4js = require('log4js');
+
+let logger = log4js.getLogger();
+let debugEnabled = process.env.DEBUG === '1';
+logger.level = debugEnabled ? 'debug' : 'info';
 
 let port = process.env.PORT || 3000;
 
@@ -10,13 +15,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    logger.info('a user connected');
     socket.emit('test message', {key: 'value'});
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        logger.info('user disconnected');
     });
 });
 
 http.listen(port, function() {
-    console.log(`listening on *:${port}`);
+    logger.info(`listening on *:${port}`);
 });
