f9cfac188e3d662b86b5a5e9df5f9147c2cc80d8
Andreas Brain <dev@abrain.de>
2020-03-10 17:56:45 +0100

    Only block connections if they do not send the required query parameters. Store info about connected, but not yet fully authenticated sockets separately.

--- socket-controller.js
+++ socket-controller.js
@@ -10,49 +10,58 @@ module.exports = class SocketController {
         this.io.on('connection', socket => this.onConnected(socket));
 
         this.sockets = new Map();
+        this.pendingDisplayIds = new Set();
     }
 
-    onConnected(socket) {
+    async onConnected(socket) {
         this.logger.debug(`Socket ${socket.id} connected`);
-        socket.emit('test message', { key: 'value' });
 
         socket.on('error', (error) => this.logger.error(`Socket ${socket.id}`, error));
         socket.once('disconnect', (reason) => this.onDisconnect(socket, reason));
 
         let displayId = socket.handshake.query.displayId;
+        let display = await this.controller.findDisplay(displayId);
+
+        if (display === null || !display.active) {
+            this.logger.warn(`Could not find an active display with id ${displayId}`);
+            socket.emit('auth_error', { message: 'Display not active' });
+            this.pendingDisplayIds.add(displayId);
+            this.sockets.set(displayId, socket);
+            return;
+        }
+
         this.sockets.set(displayId, socket);
+        socket.emit('auth_success', {});
     }
 
     onDisconnect(socket, reason) {
         this.logger.debug(`Socket ${socket.id} disconnected with reason ${reason}`);
         let displayId = socket.handshake.query.displayId;
-        this.logger.debug('Display ID', displayId);
         this.sockets.delete(displayId);
+        this.pendingDisplayIds.delete(displayId);
     }
 
     /**
-     * socket.io middleware to verify if a new socket connection should be allowed
+     * Checks if the respective Display is not yet fully authenticated.
+     *
+     * @param {String} displayId
+     * @return {boolean} True if the Display has not been fully authenticated, False otherwise.
+     */
+    isDisplayPending(displayId) {
+        return this.pendingDisplayIds.has(displayId);
+    }
+
+    /**
+     * socket.io middleware to verify if a new socket connection should be allowed (i.e. sends the required data)
      *
      * @param socket
      * @param next
-     * @return {Promise<*>}
      */
-    async verifyNewSocket(socket, next) {
-        let displayId = socket.handshake.query.displayId;
-        this.logger.debug(`Authenticating display with id ${displayId}`);
-
-        let display = await this.controller.findDisplay(displayId);
-
-        if (display === null || !display.active) {
-            this.logger.warn(`Could not find a display with id ${displayId}`);
-            return next({
-                data: {
-                    type: 'UnknownDisplay'
-                }
-            });
+    verifyNewSocket(socket, next) {
+        if (!socket.handshake.query.hasOwnProperty('displayId')) {
+            return next(new Error('Parameter displayId is missing'));
         }
 
-        this.logger.info('All good');
         return next();
     }
 };
