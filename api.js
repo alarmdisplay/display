c5966e3873033cae52b4c3221ee38ed8747d4485
Andreas Brain <dev@abrain.de>
2020-03-11 18:21:19 +0100

    Move route definitions for Displays into a separate file, put Swagger spec into JSDoc #5

--- api.js
+++ api.js
@@ -9,11 +9,8 @@ module.exports = class APIv1 {
         this.router.use(express.json());
 
         // Register the routes and their handlers
-        this.router.get('/displays', this.getDisplays);
-        this.router.post('/displays', this.postDisplays);
-        this.router.get('/displays/:id', this.getDisplay);
-        this.router.delete('/displays/:id', this.deleteDisplay);
-        this.router.put('/displays/:id', this.putDisplay);
+        let displayRoutes = require('./routes/displays')(controller);
+        this.router.use('/displays', displayRoutes);
 
         // Add our own error handler to override the built-in one
         this.router.use(this.errorHandler);
@@ -32,88 +29,4 @@ module.exports = class APIv1 {
         res.status(500).json({error: {message: err.message}});
     };
 
-    /**
-     * Returns all the Displays.
-     *
-     * @param {e.Request} req
-     * @param {e.Response} res
-     * @param {e.NextFunction} next
-     */
-    getDisplays = async (req, res, next) => {
-        try {
-            let displays = await this.controller.findDisplays();
-            res.json(displays);
-        } catch (e) {
-            return next(e);
-        }
-    };
-
-    /**
-     * Creates a Display.
-     *
-     * @param {e.Request} req
-     * @param {e.Response} res
-     * @param {e.NextFunction} next
-     */
-    postDisplays = async (req, res, next) => {
-        try {
-            let display = await this.controller.createDisplay(req.body.identifier, req.body.active, req.body.description, req.body.location);
-            let baseUrl = req.originalUrl.replace(/\/$/, '');
-            let newLocation = `${baseUrl}/${display.id}`;
-            res.set('Location', newLocation).status(201).json(display);
-        } catch (e) {
-            return next(e);
-        }
-    };
-
-    /**
-     * Returns a single Display.
-     *
-     * @param {e.Request} req
-     * @param {e.Response} res
-     * @param {e.NextFunction} next
-     */
-    getDisplay = async (req, res, next) => {
-        try {
-            let display = await this.controller.findDisplay(req.params.id);
-            if (!display) {
-                return res.sendStatus(404);
-            }
-            res.json(display);
-        } catch (e) {
-            return next(e);
-        }
-    };
-
-    /**
-     * Deletes a single Display.
-     *
-     * @param {e.Request} req
-     * @param {e.Response} res
-     * @param {e.NextFunction} next
-     */
-    deleteDisplay = async (req, res, next) => {
-        try {
-            await this.controller.deleteDisplay(req.params.id);
-            res.sendStatus(204);
-        } catch (e) {
-            return next(e);
-        }
-    };
-
-    /**
-     * Updates a single Display.
-     *
-     * @param {e.Request} req
-     * @param {e.Response} res
-     * @param {e.NextFunction} next
-     */
-    putDisplay = async (req, res, next) => {
-        try {
-            let display = await this.controller.updateDisplay(req.params.id, req.body);
-            res.json(display);
-        } catch (e) {
-            return next(e);
-        }
-    };
 };
