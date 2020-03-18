401b09432cda74f63994fa477d6d27e652002199
Andreas Brain <dev@abrain.de>
2020-03-18 12:10:10 +0100

    Store the layout of a display in the database

--- __tests__/models/display.test.js
+++ __tests__/models/display.test.js
@@ -20,12 +20,37 @@ describe('Display model', () => {
       _id: 'ABC123',
       active: true,
       description: 'A description text',
+      layout: {
+        columns: 3,
+        components: [
+          { name: 'aComponent', bounds: { colStart: 1, rowStart: 1, colEnd: 3, rowEnd: 4 } },
+          { name: 'anotherComponent', bounds: { colStart: 3, rowStart: 2, colEnd: 4, rowEnd: 3 } }
+        ],
+        rows: 5
+      },
       location: 'The Cupboard under the Stairs'
     })
     const savedDisplay = await display.save()
     expect(savedDisplay._id).toBe('ABC123')
     expect(savedDisplay.active).toBeTruthy()
     expect(savedDisplay.description).toBe('A description text')
+    expect(savedDisplay.layout).toBeInstanceOf(Object)
+    expect(savedDisplay.layout.columns).toBe(3)
+    expect(savedDisplay.layout.components).toBeInstanceOf(Array)
+    expect(savedDisplay.layout.components.length).toBe(2)
+    expect(savedDisplay.layout.components[0]._id).toBeDefined()
+    expect(savedDisplay.layout.components[0].name).toBe('aComponent')
+    expect(savedDisplay.layout.components[0].bounds.colStart).toBe(1)
+    expect(savedDisplay.layout.components[0].bounds.rowStart).toBe(1)
+    expect(savedDisplay.layout.components[0].bounds.colEnd).toBe(3)
+    expect(savedDisplay.layout.components[0].bounds.rowEnd).toBe(4)
+    expect(savedDisplay.layout.components[1]._id).toBeDefined()
+    expect(savedDisplay.layout.components[1].name).toBe('anotherComponent')
+    expect(savedDisplay.layout.components[1].bounds.colStart).toBe(3)
+    expect(savedDisplay.layout.components[1].bounds.rowStart).toBe(2)
+    expect(savedDisplay.layout.components[1].bounds.colEnd).toBe(4)
+    expect(savedDisplay.layout.components[1].bounds.rowEnd).toBe(3)
+    expect(savedDisplay.layout.rows).toBe(5)
     expect(savedDisplay.location).toBe('The Cupboard under the Stairs')
     expect(savedDisplay.createdAt).toBeInstanceOf(Date)
     expect(savedDisplay.updatedAt).toBeInstanceOf(Date)
@@ -38,6 +63,7 @@ describe('Display model', () => {
     expect(savedDisplay.active).toBeFalsy()
     expect(savedDisplay.description).toBe('')
     expect(savedDisplay.lastSeen).toBeNull()
+    expect(savedDisplay.layout).toBeNull()
     expect(savedDisplay.location).toBe('')
   })
 
--- src/models/display.js
+++ src/models/display.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const componentSchema = new Schema({
+  name: String,
+  bounds: {
+    colStart: { type: Number, min: 1 },
+    rowStart: { type: Number, min: 1 },
+    colEnd: { type: Number, min: 2 },
+    rowEnd: { type: Number, min: 2 }
+  }
+})
+
+const layoutSchema = new Schema({
+  columns: Number,
+  components: [componentSchema],
+  rows: Number
+})
+
 const displaySchema = new Schema({
   _id: { type: String },
   active: { type: Boolean, default: false },
   description: { type: String, default: '' },
-  location: { type: String, default: '' },
-  lastSeen: { type: Date, default: null }
+  lastSeen: { type: Date, default: null },
+  layout: { type: layoutSchema, default: null },
+  location: { type: String, default: '' }
 }, { timestamps: true })
 
 module.exports = mongoose.model('Display', displaySchema)
