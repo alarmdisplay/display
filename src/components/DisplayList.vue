00d53f1dad69ee447bc84bb579a06f916a64040a
Andreas Brain <dev@abrain.de>
2020-03-24 15:17:09 +0100

    Remove icons from the headers

--- src/components/DisplayList.vue
+++ src/components/DisplayList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w3-main" style="margin-left:300px;margin-top:43px;">
         <header class="w3-container" style="padding-top:22px">
-            <h2><b><font-awesome-icon icon="desktop" /> Displays</b></h2>
+            <h2><b>Displays</b></h2>
         </header>
 
         <div class="w3-row-padding w3-margin-bottom w3-margin-top">
--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w3-main" style="margin-left:300px;margin-top:43px;">
         <header class="w3-container" style="padding-top:22px">
-            <h2><b><font-awesome-icon icon="home" /> &Uuml;bersicht</b></h2>
+            <h2><b>&Uuml;bersicht</b></h2>
         </header>
     </div>
 </template>
