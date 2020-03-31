3189207cffb097ac23fe55dfde51d096fbc9b961
Andreas Brain <dev@abrain.de>
2020-03-31 21:24:39 +0200

    Show announcement details

--- src/components/announcements/Item.vue
+++ src/components/announcements/Item.vue
@@ -1,10 +1,23 @@
 <template>
-    <li>Some announcement</li>
+    <li>
+        <span class="title">{{ announcement.title }}</span><br>
+        <span class="text">{{ announcement.text }}</span>
+        <div class="date">{{ issueDate }}</div>
+    </li>
 </template>
 
 <script>
     export default {
         name: "Item",
+        computed: {
+            issueDate: function () {
+                let date = new Date(this.announcement.issued);
+                if (isNaN(date.valueOf())) {
+                    return ''
+                }
+                return this.$moment(date).format("LL")
+            }
+        },
         props: {
             announcement: Object
         }
@@ -17,5 +30,14 @@
         border-bottom: 1px solid #999;
         list-style: none;
         padding: 1em;
+    }
+
+    .date {
+        text-align: right;
+    }
+
+    .title {
+        font-size: large;
+        font-weight: bold;
     }
 </style>
