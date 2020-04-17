6d562a8963af5d123a122492d4c70b8d215f33ed
Andreas Brain <dev@abrain.de>
2020-04-17 20:28:16 +0200

    Return an array of views as a fallback instead of a single object. Also fixed whitespace issues

--- src/components/DisplayApp.vue
+++ src/components/DisplayApp.vue
@@ -11,31 +11,33 @@
             IdleScreen
         },
         methods: {
-          getViewsForScreenType: function (screenType) {
-              if (!screenType) {
-                return []
-              }
-
-              let views = this.views.filter(view => view.screenType === screenType)
-              if (views.length === 0) {
-                // Return a fallback config that just shows the clock in the center
-                return {
-                  columns: 3,
-                  rows: 3,
-                  components: [
-                    {
-                      name: 'Clock',
-                      instanceId: -1,
-                      columnStart: 2,
-                      rowStart: 2,
-                      columnEnd: 3,
-                      rowEnd: 3
-                    }
-                  ]
+            getViewsForScreenType: function (screenType) {
+                if (!screenType) {
+                  return []
                 }
-              }
 
-              return views
+                let views = this.views.filter(view => view.screenType === screenType)
+                if (views.length === 0) {
+                    // Return a fallback config that just shows the clock in the center
+                    return [
+                        {
+                            columns: 3,
+                            rows: 3,
+                            components: [
+                                {
+                                    name: 'Clock',
+                                    instanceId: -1,
+                                    columnStart: 2,
+                                    rowStart: 2,
+                                    columnEnd: 3,
+                                    rowEnd: 3
+                                }
+                            ]
+                        }
+                    ]
+                }
+
+                return views
             }
         },
         props: {
