5e48d2377e0dda62fd373e528845fb607f7cf22e
Andreas Brain <dev@abrain.de>
2020-04-20 23:02:27 +0200

    Handle also 400 and 404 error in the global API error handler

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const log4js = require('log4js')
 
+const IllegalArgumentError = require('./errors/IllegalArgumentError')
+const NotFoundError = require('./errors/NotFoundError')
+
 module.exports = class APIv1 {
   /**
    * @param {DisplayService} displayService
@@ -17,8 +20,17 @@ module.exports = class APIv1 {
     // Add our own error handler to override the built-in one
     // eslint-disable-next-line no-unused-vars
     this.router.use((err, req, res, next) => {
-      this.logger.error(err)
-      res.status(500).json({ error: { message: err.message } })
+      let statusCode = 500
+      if (err instanceof IllegalArgumentError) {
+        statusCode = 400
+      } else if (err instanceof NotFoundError) {
+        statusCode = 404
+      }
+
+      if (statusCode === 500) {
+        this.logger.error(err)
+      }
+      res.status(statusCode).json({ error: { message: err.message } })
     })
   }
 }
