e41fc0754382e30ba621d9341d41daa4d44bb37b
Andreas Brain <dev@abrain.de>
2020-04-20 18:50:42 +0200

    Repositories fot Components and Content Slots

---
+++ src/persistence/ComponentRepository.js
@@ -0,0 +1,127 @@
+const componentTypes = ['AnnouncementList', 'Clock', 'DWDWarningsMap']
+
+class ComponentRepository {
+  constructor () {
+    this.components = new Map()
+    this.instanceCounter = 1
+  }
+
+  /**
+   * Stores a new Component object.
+   *
+   * @param {String} type
+   * @param {String} name
+   *
+   * @return {Promise}
+   */
+  createComponent (type, name) {
+    return new Promise((resolve, reject) => {
+      if (!componentTypes.includes(type)) {
+        reject(new Error(`Component type '${type} is unknown'`))
+      }
+
+      const newComponent = {
+        id: this.instanceCounter++,
+        name: name,
+        type: type
+      }
+      this.components.set(newComponent.id, newComponent)
+      resolve(newComponent)
+    })
+  }
+
+  /**
+   * @return {Promise}
+   */
+  getAllComponents () {
+    return new Promise(resolve => {
+      const components = Array.from(this.components.values())
+      resolve(components)
+    })
+  }
+
+  /**
+   * Get a single Component object.
+   *
+   * @param {Number} id The ID of the Component
+   *
+   * @return {Promise}
+   */
+  getComponent (id) {
+    return new Promise((resolve, reject) => {
+      if (!this.components.has(id)) {
+        return reject(new Error(`No Component with ID ${id} found`))
+      }
+
+      resolve(this.components.get(id))
+    })
+  }
+
+  /**
+   * Get multiple Components at once.
+   *
+   * @param {Number[]} ids The IDs of the Components
+   *
+   * @return {Promise}
+   */
+  getComponents (ids) {
+    return new Promise((resolve) => {
+      const components = []
+
+      for (const id of ids) {
+        if (!this.components.has(id)) {
+          continue
+        }
+
+        components.push(this.components.get(id))
+      }
+
+      resolve(components)
+    })
+  }
+
+  /**
+   * Deletes a single Component object.
+   *
+   * @param {Number} id
+   *
+   * @return {Promise}
+   */
+  deleteComponent (id) {
+    return new Promise(resolve => {
+      const itemDidExist = this.components.delete(id)
+      if (itemDidExist) {
+        resolve(id)
+      } else {
+        resolve(undefined)
+      }
+    })
+  }
+
+  /**
+   * Updates a single Component object.
+   *
+   * @param {Number} id
+   * @param {String} type
+   * @param {String} name
+   *
+   * @return {Promise}
+   */
+  updateComponent (id, type, name) {
+    return new Promise((resolve, reject) => {
+      if (!this.components.has(id)) {
+        return reject(new Error(`No Component with ID ${id} found`))
+      }
+
+      const component = {
+        id: id,
+        name: name,
+        type: type
+      }
+      this.components.set(id, component)
+      resolve(component)
+    })
+  }
+}
+
+module.exports = ComponentRepository
---
+++ src/persistence/ContentSlotRepository.js
@@ -0,0 +1,128 @@
+class ContentSlotRepository {
+  constructor () {
+    this.contentSlots = new Map()
+    this.instanceCounter = 1
+  }
+
+  /**
+   * @param {Number} componentId
+   * @param {Number} viewId
+   * @param {Number} columnStart
+   * @param {Number} rowStart
+   * @param {Number} columnEnd
+   * @param {Number} rowEnd
+   *
+   * @return {Promise}
+   */
+  createContentSlot (componentId, viewId, columnStart, rowStart, columnEnd, rowEnd) {
+    return new Promise(resolve => {
+      const newContentSlot = {
+        id: this.instanceCounter++,
+        componentId: componentId,
+        viewId: viewId,
+        columnStart: columnStart,
+        rowStart: rowStart,
+        columnEnd: columnEnd,
+        rowEnd: rowEnd
+      }
+      this.contentSlots.set(newContentSlot.id, newContentSlot)
+      resolve(newContentSlot)
+    })
+  }
+
+  /**
+   * @return {Promise}
+   */
+  getAllContentSlots () {
+    return new Promise(resolve => {
+      const contentSlots = Array.from(this.contentSlots.values())
+      resolve(contentSlots)
+    })
+  }
+
+  /**
+   * @param {Number} id
+   *
+   * @return {Promise}
+   */
+  getContentSlot (id) {
+    return new Promise((resolve, reject) => {
+      if (!this.contentSlots.has(id)) {
+        return reject(new Error(`No Content Slot with ID ${id} found`))
+      }
+
+      resolve(this.contentSlots.get(id))
+    })
+  }
+
+  /**
+   * Finds and returns Content Slot objects that belong to a certain View.
+   *
+   * @param {Number} viewId The ID of the View
+   *
+   * @return {Promise}
+   */
+  getContentSlotsByViewId (viewId) {
+    return new Promise((resolve) => {
+      const contentSlots = []
+      for (const contentSlot of this.contentSlots.values()) {
+        if (contentSlot.viewId === viewId) {
+          contentSlots.push(contentSlot)
+        }
+      }
+
+      resolve(contentSlots)
+    })
+  }
+
+  /**
+   * Deletes a Content Slot object.
+   *
+   * @param {Number} id The ID of the Content Slot
+   *
+   * @return {Promise}
+   */
+  deleteContentSlot (id) {
+    return new Promise(resolve => {
+      const itemDidExist = this.contentSlots.delete(id)
+      if (itemDidExist) {
+        resolve(id)
+      } else {
+        resolve(undefined)
+      }
+    })
+  }
+
+  /**
+   * @param {Number} id
+   * @param {Number} componentId
+   * @param {Number} viewId
+   * @param {Number} columnStart
+   * @param {Number} rowStart
+   * @param {Number} columnEnd
+   * @param {Number} rowEnd
+   *
+   * @return {Promise}
+   */
+  updateContentSlot (id, componentId, viewId, columnStart, rowStart, columnEnd, rowEnd) {
+    return new Promise((resolve, reject) => {
+      if (!this.contentSlots.has(id)) {
+        return reject(new Error(`No Content Slot with ID ${id} found`))
+      }
+
+      const contentSlot = {
+        id: id,
+        componentId: componentId,
+        viewId: viewId,
+        columnStart: columnStart,
+        rowStart: rowStart,
+        columnEnd: columnEnd,
+        rowEnd: rowEnd
+      }
+      this.contentSlots.set(id, contentSlot)
+      resolve(contentSlot)
+    })
+  }
+}
+
+module.exports = ContentSlotRepository
