8318d86d7938f4f35edfb40450832d2385cbfb38
Andreas Brain <dev@abrain.de>
2020-04-28 17:59:56 +0200

    Documented the API to manage Components

--- src/routes/components.js
+++ src/routes/components.js
@@ -5,6 +5,53 @@ const router = express.Router()
  * @param {ComponentService} componentService
  */
 module.exports = function (componentService) {
+  /**
+   * @swagger
+   * definitions:
+   *   Component:
+   *     type: object
+   *     required:
+   *     - name
+   *     - type
+   *     properties:
+   *       id:
+   *         type: number
+   *         readOnly: true
+   *       name:
+   *         type: string
+   *       type:
+   *         type: string
+   *         enum:
+   *         - AnnouncementList
+   *         - Clock
+   *         - DWDWarningMap
+   */
+
+  /**
+   * @swagger
+   * /api/v1/components/:
+   *   post:
+   *     summary: Create a new Component
+   *     produces: application/json
+   *     parameters:
+   *       - name: component
+   *         in: body
+   *         required: true
+   *         description: Fields for the Component resource
+   *         schema:
+   *           $ref: '#/definitions/Component'
+   *     responses:
+   *       201:
+   *         description: The newly created Component
+   *         schema:
+   *           $ref: '#/definitions/Component'
+   *         headers:
+   *           Location:
+   *             description: The URI of the newly created Component resource
+   *             type: string
+   *     tags:
+   *       - Components
+   */
   router.post('/', (req, res, next) => {
     componentService.createComponent(req.body.type || '', req.body.name || '')
       .then(component => {
@@ -15,6 +62,22 @@ module.exports = function (componentService) {
       .catch(error => next(error))
   })
 
+  /**
+   * @swagger
+   * /api/v1/components/:
+   *   get:
+   *     summary: Returns all Components
+   *     produces: application/json
+   *     responses:
+   *       200:
+   *         description: All available Components
+   *         schema:
+   *           type: array
+   *           items:
+   *             $ref: '#/definitions/Component'
+   *     tags:
+   *       - Components
+   */
   router.get('/', (req, res, next) => {
     componentService.getAllComponents()
       .then(components => {
@@ -23,6 +86,27 @@ module.exports = function (componentService) {
       .catch(error => next(error))
   })
 
+  /**
+   * @swagger
+   * /api/v1/components/{id}:
+   *   get:
+   *     summary: Returns a single Component
+   *     produces: application/json
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         description: The ID of the Component
+   *         type: number
+   *     responses:
+   *       200:
+   *         description: The Component exists and gets returned
+   *         schema:
+   *           $ref: '#/definitions/Component'
+   *       404:
+   *         description: The Component could not be found
+   *     tags:
+   *       - Components
+   */
   router.get('/:id', (req, res, next) => {
     componentService.getComponent(parseInt(req.params.id))
       .then(component => {
@@ -31,6 +115,36 @@ module.exports = function (componentService) {
       .catch(error => next(error))
   })
 
+  /**
+   * @swagger
+   * /api/v1/components/{id}:
+   *   put:
+   *     summary: Updates a single Component
+   *     produces: application/json
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         description: The ID of the Component
+   *         required: true
+   *         type: number
+   *       - name: component
+   *         in: body
+   *         description: Fields for the Component resource
+   *         schema:
+   *           type: object
+   *           properties:
+   *             name:
+   *               type: string
+   *     responses:
+   *       200:
+   *         description: Successfully updated
+   *         schema:
+   *           $ref: '#/definitions/Component'
+   *       404:
+   *         description: Component does not exist and cannot be updated. Please use POST to create a new Component
+   *     tags:
+   *       - Components
+   */
   router.put('/:id', (req, res, next) => {
     componentService.getComponent(parseInt(req.params.id))
       .then(component => componentService.updateComponent(component.id, req.body.name || ''))
@@ -38,6 +152,23 @@ module.exports = function (componentService) {
       .catch(error => next(error))
   })
 
+  /**
+   * @swagger
+   * /api/v1/components/{id}:
+   *   delete:
+   *     summary: Deletes a single Component
+   *     produces: application/json
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         description: The ID of the Component
+   *         type: string
+   *     responses:
+   *       204:
+   *         description: Successfully deleted
+   *     tags:
+   *       - Components
+   */
   router.delete('/:id', (req, res, next) => {
     componentService.deleteComponent(parseInt(req.params.id))
       .then(() => res.sendStatus(204))
