c3ac4e1f8ee1ecb5677c87c95eca662ae4acea30
Andreas Brain <dev@abrain.de>
2020-05-04 14:10:31 +0200

    Center the setup instructions again

--- src/components/DisplaySetup.vue
+++ src/components/DisplaySetup.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <h1>Setup</h1>
-        <p>Dieses Display scheint noch nicht eingerichtet worden zu sein</p>
-        <p>Bitte stelle sicher, dass ein Display mit der folgenden ID in der Console eingerichtet und aktiviert ist.</p>
+    <div class="setup-instructions">
+        <h1>Willkommen</h1>
+        <p>Dieses Display scheint noch nicht eingerichtet worden zu sein.</p>
+        <p>Bitte stelle in der Console sicher, dass ein Display mit der folgenden Client-ID eingerichtet und aktiviert ist.</p>
         <p class="display-identifier">{{ displayIdentifier }}</p>
     </div>
 </template>
@@ -17,7 +17,13 @@
 </script>
 
 <style scoped>
+.setup-instructions {
+    font-size: 2em;
+    padding: 1em 1em;
+    text-align: center;
+}
+
 .display-identifier {
-    font-size: xx-large;
+    font-size: 2.5em;
 }
 </style>
