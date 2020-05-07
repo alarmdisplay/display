080c1f5599e7c8837a8e7aeeb8ecfc1fa9aeaa12
Andreas Brain <dev@abrain.de>
2020-05-07 16:04:46 +0200

    Show basic alert data

--- src/components/DefaultAlertView.vue
+++ src/components/DefaultAlertView.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="alert">
-        <p>{{ alert.title }}</p>
-        <p>{{ alert.time }}</p>
+        <div class="info">
+            <p class="title">{{ alert.title }}</p>
+            <span class="subtitle">{{ alert.subtitle }}</span>
+            <p class="address">{{ alert.location.raw || '' }}</p>
+            <p class="description">{{ alert.description }}</p>
+        </div>
     </div>
 </template>
 
@@ -16,7 +20,40 @@
 
 <style scoped>
     .alert {
+        background-color: #eee;
         height: 100%;
+        overflow: hidden;
         width: 100%;
+        display: grid;
+        place-content: space-evenly;
+        grid-template-columns: 90%;
+        grid-template-rows: 90%;
+    }
+
+    .info {
+        padding: 0 2em;
+    }
+
+    .title {
+        font-size: 4em;
+    }
+
+    .subtitle {
+        background-color: orange;
+        border-radius: 10px;
+        font-size: 2.5em;
+        padding: 0.3em;
+    }
+
+    .address {
+        font-size: 3em;
+    }
+
+    .description {
+        font-family: "Courier New", monospace;
+        font-size: 2.5em;
+        border: 1px solid black;
+        background-color: aliceblue;
+        padding: 0.4em;
     }
 </style>
