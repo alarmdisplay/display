374714800b038fba2b997d332415d95b9ea440f2
Andreas Brain <dev@abrain.de>
2020-05-22 11:52:38 +0200

    Use an open font

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
