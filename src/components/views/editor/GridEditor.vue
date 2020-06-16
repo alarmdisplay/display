a1637f9153f553ca96923d7f02fc59db9c58b661
Andreas Brain <dev@abrain.de>
2020-06-16 19:26:55 +0200

    When the content slot is dropped, calculate the column and row in which the

--- src/components/views/editor/GridEditor.vue
+++ src/components/views/editor/GridEditor.vue
@@ -34,7 +34,21 @@ export default {
     onDrop: function (event) {
       console.log(event)
       const data = event.dataTransfer.getData('application/json')
-      console.log(JSON.parse(data))
+      const contentSlot = JSON.parse(data)
+      console.log(contentSlot)
+
+      // Calculate the X and Y coordinates relative to the drop zone
+      const clientRect = event.target.getBoundingClientRect()
+      const x = event.clientX - clientRect.left
+      const y = event.clientY - clientRect.top
+      console.log('Content Slot', contentSlot.id, 'dropped at', x, y)
+
+      // Calculate in which column and row the item has been dropped
+      const columnWidth = clientRect.width / this.viewData.columns
+      const rowHeight = clientRect.height / this.viewData.rows
+      const column = Math.floor(x / columnWidth) + 1
+      const row = Math.floor(y / rowHeight) + 1
+      console.log(`Content Slot has been dropped in column ${column} and row ${row}`)
     }
   },
   props: {
