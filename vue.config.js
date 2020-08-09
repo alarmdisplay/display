cc1b06ec63aa58e38a56201630bf27cbea366f2e
Andreas Brain <dev@abrain.de>
2020-08-09 20:10:09 +0200

    Adjust the backend URL

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   devServer: {
-      proxy: 'http://localhost:3000/'
+      proxy: 'http://localhost:3031/'
   },
   publicPath: process.env.NODE_ENV === 'production' ? '/display/' : '/'
 };
