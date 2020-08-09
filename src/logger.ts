335ea8adbb77ed9a8173aca62c12edf720ccb6ed
Andreas Brain <dev@abrain.de>
2020-08-09 17:25:49 +0200

    Replace winston with log4js

--- src/logger.ts
+++ src/logger.ts
@@ -1,16 +1,6 @@
-import { createLogger, format, transports } from 'winston';
+import { getLogger } from 'log4js';
 
-// Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
-const logger = createLogger({
-  // To see more detailed errors, change this to 'debug'
-  level: 'info',
-  format: format.combine(
-    format.splat(),
-    format.simple()
-  ),
-  transports: [
-    new transports.Console()
-  ],
-});
+const logger = getLogger();
+logger.level = 'debug';
 
 export default logger;
