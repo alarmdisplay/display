4383d21d84fd6e0aaaa2e99ebbf869f8f3b4d3a9
Andreas Brain <dev@abrain.de>
2020-08-12 13:38:46 +0200

    Allow API key authentication for the incidents service

--- src/services/incidents/incidents.hooks.ts
+++ src/services/incidents/incidents.hooks.ts
@@ -1,11 +1,12 @@
 import * as authentication from '@feathersjs/authentication';
+import { allowApiKey } from '../../hooks/allowApiKey';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
 
 export default {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [ allowApiKey(), authenticate('jwt', 'api-key') ],
     find: [],
     get: [],
     create: [],
