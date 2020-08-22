59544e0d83c3cf4e99e43d4c40008b2e097725f1
Andreas Brain <dev@abrain.de>
2020-08-22 20:29:25 +0200

    Make sure the API key header is never empty

--- src/feathers-client.js
+++ src/feathers-client.js
@@ -3,18 +3,21 @@ import socketio from '@feathersjs/socketio-client'
 import io from 'socket.io-client'
 import feathersVuex from 'feathers-vuex'
 
-let options = {};
 let apiKey = localStorage.getItem('display-api-key');
-
-if (apiKey) {
-    options.transportOptions = {
+if (!apiKey || apiKey === '') {
+    // Make sure the header is not empty, so it gets sent and the authentication process kicks in
+    apiKey = 'none'
+}
+let options = {
+    transportOptions: {
         polling: {
             extraHeaders: {
                 'x-api-key': apiKey
             }
         }
     }
-}
+};
+
 const socket = io(options)
 
 const feathersClient = feathers()
