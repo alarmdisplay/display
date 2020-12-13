d98aa61e621cf6f3d106990aebc3dc4f611cd03d
Andreas Brain <dev@abrain.de>
2020-12-13 17:41:50 +0100

    Exit the process if the database is not available

--- server/src/sequelize.ts
+++ server/src/sequelize.ts
@@ -44,12 +44,19 @@ export default function (app: Application): void {
     });
 
     // Migrate and sync to the database
-    app.set('sequelizeSync', umzug.up().then(() => {
+    app.set('sequelizeSync', sequelize.authenticate().then(() => {
+      logger.info('Connected to database');
+      return umzug.up();
+    }, (reason: Error) => {
+      logger.error('Database connection failed:', reason.message);
+      process.exit(1);
+    }).then(() => {
       logger.info('Database migration successful');
       // Resolve the databaseReady Promise
       app.get('databaseReadyResolve')();
     }).catch((reason: Error) => {
       logger.error('Database migration failed:', reason.message);
+      process.exit(2);
     }));
 
     return result;
