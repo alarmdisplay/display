b9ddb121ac392d4e46a3506caf75338e40f4006b
Andreas Brain <dev@abrain.de>
2021-09-30 16:58:31 +0200

    Ensure default options for content slots

--- server/src/services/content-slots/content-slots.hooks.ts
+++ server/src/services/content-slots/content-slots.hooks.ts
@@ -1,6 +1,9 @@
 import * as authentication from '@feathersjs/authentication';
 import { allowApiKey } from '../../hooks/allowApiKey';
 import { unserializeJson } from '../../hooks/unserializeJson';
+import { HookContext } from '@feathersjs/feathers';
+import { getItems, replaceItems } from 'feathers-hooks-common';
+import { ContentSlotData } from '../../declarations';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
@@ -17,7 +20,7 @@ export default {
   },
 
   after: {
-    all: [ unserializeJson('options') ],
+    all: [ unserializeJson('options'), ensureDefaultOptions ],
     find: [],
     get: [],
     create: [],
@@ -36,3 +39,22 @@ export default {
     remove: []
   }
 };
+
+const defaultOptions = new Map<string, any>([
+  ['AnnouncementList', { title: '' }],
+  ['DWDWarningMap', { areaCode: 'DE', mapType: 'area' }]
+]);
+
+function ensureDefaultOptions(context: HookContext): HookContext {
+  const items = getItems(context) as ContentSlotData|ContentSlotData[];
+  if (Array.isArray(items)) {
+    items.forEach(item => {
+      item.options = Object.assign({}, defaultOptions.get(item.component) || {}, item.options);
+    });
+  } else {
+    items.options = Object.assign({}, defaultOptions.get(items.component) || {}, items.options);
+  }
+
+  replaceItems(context, items);
+  return context;
+}
