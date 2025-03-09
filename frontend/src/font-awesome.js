7c928c60ee84508e5f2202efcb4efab670054055
Andreas Brain <dev@abrain.de>
2025-03-10 00:41:36 +0100

    Move Font Awesome initialization to separate file

---
+++ frontend/src/font-awesome.js
@@ -0,0 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import {
+  faBullhorn,
+  faCalendar,
+  faClock,
+  faStopwatch
+} from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faBullhorn,
+  faCalendar,
+  faClock,
+  faStopwatch,
+);
+
+export default FontAwesomeIcon
